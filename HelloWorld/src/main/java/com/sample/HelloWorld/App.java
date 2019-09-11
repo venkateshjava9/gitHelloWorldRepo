bcb19b6e90cb0315fd73847a48860b0ef83312d6
venky <venkatesh.java9@gmail.com>
2019-09-10 20:26:52 -0700

    Added Sum Functionality

--- HelloWorld/src/main/java/com/sample/HelloWorld/App.java
+++ HelloWorld/src/main/java/com/sample/HelloWorld/App.java
@@ -6,10 +6,19 @@ package com.sample.HelloWorld;
  */
 public class App 
 {
+	// Sum of two numbers
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	
     public static void main( String[] args )
     {
     	// Hello World 
     	//Added one more comment
         System.out.println( "Hello World!" );
+        App app = new App();
+        int val = app.sum(10, 20);
+        System.out.println("value is" +val);
     }
 }
